af5191760f910b4d3a453f406b9a1f8ffa5b0b55
Brian Bidulock <bidulock@openss7.org>
2014-01-12 14:50:34 -0700

    initial commit off skeleton

---
+++ xdg-launch.c
@@ -0,0 +1,451 @@
+/*****************************************************************************
+
+ @(#) $RCSfile$ $Name$($Revision$) $Date$
+
+ -----------------------------------------------------------------------------
+
+ Copyright (c) 2008-2014  Monavacon Limited <http://www.monavacon.com/>
+ Copyright (c) 2001-2008  OpenSS7 Corporation <http://www.openss7.com/>
+ Copyright (c) 1997-2001  Brian F. G. Bidulock <bidulock@openss7.org>
+
+ All Rights Reserved.
+
+ This program is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation, version 3 of the license.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program.  If not, see <http://www.gnu.org/licenses/>, or write to the
+ Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+ -----------------------------------------------------------------------------
+
+ U.S. GOVERNMENT RESTRICTED RIGHTS.  If you are licensing this Software on
+ behalf of the U.S. Government ("Government"), the following provisions apply
+ to you.  If the Software is supplied by the Department of Defense ("DoD"), it
+ is classified as "Commercial Computer Software" under paragraph 252.227-7014
+ of the DoD Supplement to the Federal Acquisition Regulations ("DFARS") (or any
+ successor regulations) and the Government is acquiring only the license rights
+ granted herein (the license rights customarily provided to non-Government
+ users).  If the Software is supplied to any unit or agency of the Government
+ other than DoD, it is classified as "Restricted Computer Software" and the
+ Government's rights in the Software are defined in paragraph 52.227-19 of the
+ Federal Acquisition Regulations ("FAR") (or any successor regulations) or, in
+ the cases of NASA, in paragraph 18.52.227-86 of the NASA Supplement to the FAR
+ (or any successor regulations).
+
+ -----------------------------------------------------------------------------
+
+ Commercial licensing and support of this software is available from OpenSS7
+ Corporation at a fee.  See http://www.openss7.com/
+
+ -----------------------------------------------------------------------------
+
+ Last Modified $Date$ by $Author$
+
+ -----------------------------------------------------------------------------
+
+ $Log$
+ *****************************************************************************/
+
+#ident "@(#) $RCSfile$ $Name$($Revision$) $Date$"
+
+static char const ident[] = "$RCSfile$ $Name$($Revision$) $Date$";
+
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 600
+#endif
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#ifdef _GNU_SOURCE
+#include <getopt.h>
+#endif
+#include <time.h>
+#include <signal.h>
+#include <syslog.h>
+#include <sys/utsname.h>
+
+#include <assert.h>
+#include <locale.h>
+#include <stdarg.h>
+#include <strings.h>
+#include <regex.h>
+
+#include <X11/Xatom.h>
+#include <X11/Xlib.h>
+#include <X11/Xproto.h>
+#include <X11/Xutil.h>
+#include <X11/Xresource.h>
+#ifdef XRANDR
+#include <X11/extensions/Xrandr.h>
+#include <X11/extensions/randr.h>
+#endif
+#ifdef XINERAMA
+#include <X11/extensions/Xinerama.h>
+#endif
+#ifdef STARTUP_NOTIFICATION
+#define SN_API_NOT_YET_FROZEN
+#include <libsn/sn.h>
+#endif
+
+const char *program = NAME;
+
+static int debug = 0;
+static int output = 1;
+
+static void
+copying(int argc, char *argv[])
+{
+	if (!output && !debug)
+		return;
+	(void) fprintf(stdout, "\
+--------------------------------------------------------------------------------\n\
+%1$s\n\
+--------------------------------------------------------------------------------\n\
+Copyright (c) 2008-2014  Monavacon Limited <http://www.monavacon.com/>\n\
+Copyright (c) 2001-2008  OpenSS7 Corporation <http://www.openss7.com/>\n\
+Copyright (c) 1997-2001  Brian F. G. Bidulock <bidulock@openss7.org>\n\
+\n\
+All Rights Reserved.\n\
+--------------------------------------------------------------------------------\n\
+This program is free software: you can  redistribute it  and/or modify  it under\n\
+the terms of the  GNU Affero  General  Public  License  as published by the Free\n\
+Software Foundation, version 3 of the license.\n\
+\n\
+This program is distributed in the hope that it will  be useful, but WITHOUT ANY\n\
+WARRANTY; without even  the implied warranty of MERCHANTABILITY or FITNESS FOR A\n\
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.\n\
+\n\
+You should have received a copy of the  GNU Affero General Public License  along\n\
+with this program.   If not, see <http://www.gnu.org/licenses/>, or write to the\n\
+Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.\n\
+--------------------------------------------------------------------------------\n\
+U.S. GOVERNMENT RESTRICTED RIGHTS.  If you are licensing this Software on behalf\n\
+of the U.S. Government (\"Government\"), the following provisions apply to you. If\n\
+the Software is supplied by the Department of Defense (\"DoD\"), it is classified\n\
+as \"Commercial  Computer  Software\"  under  paragraph  252.227-7014  of the  DoD\n\
+Supplement  to the  Federal Acquisition Regulations  (\"DFARS\") (or any successor\n\
+regulations) and the  Government  is acquiring  only the  license rights granted\n\
+herein (the license rights customarily provided to non-Government users). If the\n\
+Software is supplied to any unit or agency of the Government  other than DoD, it\n\
+is  classified as  \"Restricted Computer Software\" and the Government's rights in\n\
+the Software  are defined  in  paragraph 52.227-19  of the  Federal  Acquisition\n\
+Regulations (\"FAR\")  (or any successor regulations) or, in the cases of NASA, in\n\
+paragraph  18.52.227-86 of  the  NASA  Supplement  to the FAR (or any  successor\n\
+regulations).\n\
+--------------------------------------------------------------------------------\n\
+Commercial  licensing  and  support of this  software is  available from OpenSS7\n\
+Corporation at a fee.  See http://www.openss7.com/\n\
+--------------------------------------------------------------------------------\n\
+", ident);
+}
+
+static void
+version(int argc, char *argv[])
+{
+	if (!output && !debug)
+		return;
+	(void) fprintf(stdout, "\
+%1$s (OpenSS7 %2$s) %3$s (%4$s)\n\
+Written by Brian Bidulock.\n\
+\n\
+Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2014  Monavacon Limited.\n\
+Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008  OpenSS7 Corporation.\n\
+Copyright (c) 1997, 1998, 1999, 2000, 2001  Brian F. G. Bidulock.\n\
+This is free software; see the source for copying conditions.  There is NO\n\
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\
+\n\
+Distributed by OpenSS7 under GNU Affero General Public License Version 3,\n\
+with conditions, incorporated herein by reference.\n\
+\n\
+See `%1$s --copying' for copying permissions.\n\
+", NAME, PACKAGE, VERSION, "$Revision: 1.1.2.4 $ $Date: 2011-05-31 09:46:19 $");
+}
+
+static void
+usage(int argc, char *argv[])
+{
+	if (!output && !debug)
+		return;
+	(void) fprintf(stderr, "\
+Usage:\n\
+    %1$s [options] [APPID [FILE|URL]]\n\
+    %1$s {-h|--help}\n\
+    %1$s {-V|--version}\n\
+    %1$s {-C|--copying}\n\
+", argv[0]);
+}
+
+char *appid = "[APPID]";
+char *launcher = NAME;
+char *launchee = "[APPID]";
+unsigned int sequence = 0;
+char *hostname = "gethostname()";
+unsigned int monitor = 0;
+unsigned int screen = 0;
+unsigned int desktop = 0;
+unsigned long timestamp = 0;
+char *name = "[Name=]";
+char *icon = "[Icon=]";
+char *binary = "[TryExec=|Exec=]";
+char *description = "[Comment=]";
+char *wmclass = "[StartupWMClass=]";
+unsigned int silent = 0;
+char *file = "";
+char *url = "";
+
+static void
+help(int argc, char *argv[])
+{
+	if (!output && !debug)
+		return;
+	(void) fprintf(stdout, "\
+Usage:\n\
+    %1$s [options] [APPID [FILE|URL]]\n\
+    %1$s {-h|--help}\n\
+    %1$s {-V|--version}\n\
+    %1$s {-C|--copying}\n\
+Arguments:\n\
+    APPID\n\
+        the application identifier of XDG application to launch\n\
+    FILE\n\
+        the file name with which to launch the application\n\
+    URL\n\
+        the URL with which to launch the application\n\
+Options:\n\
+    -L, --launcher LAUNCHER\n\
+        name of launcher for startup id, default: '%2$s'\n\
+    -l, --launchee LAUNCHEE\n\
+        name of launchee for startup id, default: APPID\n\
+    -S, --sequence SEQUENCE\n\
+        sequence number to use in startup id, default: %3$u\n\
+    -n, --hostname HOSTNAME\n\
+        hostname to use in startup id, default: '%4$s'\n\
+    -m, --monitor MONITOR\n\
+        Xinerama monitor to specify in SCREEN tag, default: %5$u\n\
+    -s, --screen SCREEN\n\
+        screen to specify in SCREEN tag, default: %6$u\n\
+    -w, --workspace DESKTOP\n\
+        workspace to specify in DESKTOP tag, default: %7$u\n\
+    -t, --timestamp TIMESTAMP\n\
+        X server timestamp for startup id, default: %8$lu\n\
+    -N, --name NAME\n\
+        name of XDG application, default: '%9$s'\n\
+    -i, --icon ICON\n\
+        icon name of the XDG application, default: '%10$s'\n\
+    -b, --binary BINARY\n\
+        binary name of the XDG application, default: '%11$s'\n\
+    -D, --description DESCRIPTION\n\
+        description of the XDG application, default: '%12$s'\n\
+    -W, --wmclass WMCLASS\n\
+        resource name or class of the XDG application, default: '%13$s'\n\
+    -q, --silent SILENT\n\
+        whether startup notification is silent (0/1), default: '%14$d'\n\
+    -a, --appid APPID\n\
+        override application identifier\n\
+    -x, --exec EXEC\n\
+        override command to execute\n\
+    -f, --file FILE\n\
+        filename to use with application\n\
+    -u, --url URL\n\
+        URL to use with application\n\
+    -D, --debug [LEVEL]\n\
+        increment or set debug LEVEL [default: 0]\n\
+    -v, --verbose [LEVEL]\n\
+        increment or set output verbosity LEVEL [default: 1]\n\
+        this option may be repeated.\n\
+    -h, --help, -?, --?\n\
+        print this usage information and exit\n\
+    -V, --version\n\
+        print version and exit\n\
+    -C, --copying\n\
+        print copying permission and exit\n\
+", argv[0], launcher, sequence, hostname, monitor, screen, desktop, timestamp,
+		name, icon, binary, description, wmclass, silent);
+}
+
+int
+main(int argc, char *argv[])
+{
+	while (1) {
+		int c, val;
+
+#ifdef _GNU_SOURCE
+		int option_index = 0;
+		/* *INDENT-OFF* */
+		static struct option long_options[] = {
+			{"launcher",	required_argument,	NULL, 'L'},
+			{"launchee",	required_argument,	NULL, 'l'},
+			{"sequence",	required_argument,	NULL, 'S'},
+			{"hostname",	required_argument,	NULL, 'n'},
+			{"monitor",	required_argument,	NULL, 'm'},
+			{"screen",	required_argument,	NULL, 's'},
+			{"workspace",	required_argument,	NULL, 'w'},
+			{"timestamp",	required_argument,	NULL, 't'},
+			{"name",	required_argument,	NULL, 'N'},
+			{"icon",	required_argument,	NULL, 'i'},
+			{"binary",	required_argument,	NULL, 'b'},
+			{"description",	required_argument,	NULL, 'd'},
+			{"wmclass",	required_argument,	NULL, 'W'},
+			{"silent",	required_argument,	NULL, 'q'},
+			{"appid",	required_argument,	NULL, 'a'},
+			{"exec",	required_argument,	NULL, 'x'},
+			{"file",	required_argument,	NULL, 'f'},
+			{"url",		required_argument,	NULL, 'u'},
+
+			{"debug",	optional_argument,	NULL, 'D'},
+			{"verbose",	optional_argument,	NULL, 'v'},
+			{"help",	no_argument,		NULL, 'h'},
+			{"version",	no_argument,		NULL, 'V'},
+			{"copying",	no_argument,		NULL, 'C'},
+			{"?",		no_argument,		NULL, 'H'},
+			{ 0, }
+		};
+		/* *INDENT-ON* */
+
+		c = getopt_long(argc, argv, "L:l:S:n:m:s:w:t:N:i:b:d:W:q:a:x:f:u:D::v::hVCH?",
+				long_options, &option_index);
+#else				/* defined _GNU_SOURCE */
+		c = getopt(argc, argv, "L:l:S:n:m:s:w:t:N:i:b:d:W:q:a:x:f:u:DvhVC?");
+#endif				/* defined _GNU_SOURCE */
+		if (c == -1) {
+			if (debug)
+				fprintf(stderr, "%s: done options processing\n", argv[0]);
+			break;
+		}
+		switch (c) {
+		case 0:
+			goto bad_usage;
+		case 'L':	/* -L, --lancher LAUNCHER */
+			break;
+		case 'l':	/* -l, --launchee LAUNCHEE */
+			break;
+		case 'S':	/* -S, --sequence SEQUENCE */
+			break;
+		case 'n':	/* -n, --hostname HOSTNAME */
+			break;
+		case 'm':	/* -m, --monitor MONITOR */
+			break;
+		case 's':	/* -s, --screen SCREEN */
+			break;
+		case 'w':	/* -w, --workspace WORKSPACE */
+			break;
+		case 't':	/* -t, --timestamp TIMESTAMP */
+			break;
+		case 'N':	/* -N, --name NAME */
+			break;
+		case 'i':	/* -i, --icon ICON */
+			break;
+		case 'b':	/* -b, --binary BINARY */
+			break;
+		case 'd':	/* -d, --description DESCRIPTION */
+			break;
+		case 'W':	/* -W, --wmclass WMCLASS */
+			break;
+		case 'q':	/* -q, --silent SILENT */
+			break;
+		case 'a':	/* -a, --appid APPID */
+			break;
+		case 'x':	/* -x, --exec EXEC */
+			break;
+		case 'f':	/* -f, --file FILE */
+			break;
+		case 'u':	/* -u, --url URL */
+			break;
+
+		case 'D':	/* -D, --debug [level] */
+			if (debug)
+				fprintf(stderr, "%s: increasing debug verbosity\n", argv[0]);
+			if (optarg == NULL) {
+				debug++;
+			} else {
+				if ((val = strtol(optarg, NULL, 0)) < 0)
+					goto bad_option;
+				debug = val;
+			}
+			break;
+		case 'v':	/* -v, --verbose [level] */
+			if (debug)
+				fprintf(stderr, "%s: increasing output verbosity\n", argv[0]);
+			if (optarg == NULL) {
+				output++;
+				break;
+			}
+			if ((val = strtol(optarg, NULL, 0)) < 0)
+				goto bad_option;
+			output = val;
+			break;
+		case 'h':	/* -h, --help */
+		case 'H':	/* -H, --? */
+			if (debug)
+				fprintf(stderr, "%s: printing help message\n", argv[0]);
+			help(argc, argv);
+			exit(0);
+		case 'V':	/* -V, --version */
+			if (debug)
+				fprintf(stderr, "%s: printing version message\n", argv[0]);
+			version(argc, argv);
+			exit(0);
+		case 'C':	/* -C, --copying */
+			if (debug)
+				fprintf(stderr, "%s: printing copying message\n", argv[0]);
+			copying(argc, argv);
+			exit(0);
+		case '?':
+		default:
+		      bad_option:
+			optind--;
+		      bad_nonopt:
+			if (output || debug) {
+				if (optind < argc) {
+					fprintf(stderr, "%s: syntax error near '", argv[0]);
+					while (optind < argc)
+						fprintf(stderr, "%s ", argv[optind++]);
+					fprintf(stderr, "'\n");
+				} else {
+					fprintf(stderr, "%s: missing option or argument", argv[0]);
+					fprintf(stderr, "\n");
+				}
+				fflush(stderr);
+			      bad_usage:
+				usage(argc, argv);
+			}
+			exit(2);
+		}
+	}
+	if (debug) {
+		fprintf(stderr, "%s: option index = %d\n", argv[0], optind);
+		fprintf(stderr, "%s: option count = %d\n", argv[0], argc);
+	}
+	if (optind < argc) {
+		appid = strdup(argv[optind++]);
+		if (optind < argc) {
+			if (strchr(argv[optind], ':'))
+				url = strdup(argv[optind++]);
+			else
+				file = strdup(argv[optind++]);
+			if (optind > argc)
+				goto bad_nonopt;
+		}
+	}
+	exit(0);
+}
