8c5820af03dedf3a384ac06e7399c2ff860ad42a
Brian Bidulock <bidulock@openss7.org>
2016-07-10 02:39:15 -0600

    improve distribution process

--- autogen.sh
+++ autogen.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-PACKAGE=xdg-launch
-VERSION=
-DATE="`date -uI`"
+cd "$(dirname "$0")"
 
-if [ -z "$VERSION" ]; then
-	VERSION='1.1'
-	if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
-		VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
-		DATE=$(git show -s --format=%ci HEAD^{commit}|awk '{print$1}')
-	fi
+PACKAGE=$(grep AC_INIT configure.ac|head -1|sed -r 's,AC_INIT[(][[],,;s,[]].*,,')
+
+GTVERSION=$(gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]*\.[^\.]*)\..*$,\1,')
+
+if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
+	VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
+	DATE=$(git show -s --format=%ci HEAD^{commit}|awk '{print$1}')
+	sed -i.bak configure.ac -r \
+		-e "s:AC_INIT\([[]$PACKAGE[]],[[][^]]*[]]:AC_INIT([$PACKAGE],[$VERSION]:
+		    s:AC_REVISION\([[][^]]*[]]\):AC_REVISION([$VERSION]):
+		    s:^DATE=.*$:DATE='$DATE':
+		    s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):"
+else
+	sed -i.bak configure.ac -r \
+		-e "s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):"
 fi
-
-GTVERSION=`gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]*\.[^\.]*)\..*$,\1,'`
-
-sed -i.bak configure.ac -r \
-	-e "s:AC_INIT\([[]$PACKAGE[]],[[][^]]*[]]:AC_INIT([$PACKAGE],[$VERSION]:
-	    s:AC_REVISION\([[][^]]*[]]\):AC_REVISION([$VERSION]):
-	    s:^DATE=.*$:DATE='$DATE':
-	    s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):"
 
 mkdir m4 2>/dev/null
 
