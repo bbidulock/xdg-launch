a12829aa0acefedc5db0daf4e6d8886efcfe51eb
Brian Bidulock <bidulock@openss7.org>
2020-02-08 20:56:21 -0700

    simplify MDOCDATE and cscope

--- configure.sh
+++ configure.sh
@@ -29,12 +29,9 @@ esac
 	--enable-maintainer-mode \
 	--enable-dependency-tracking \
 	CPPFLAGS="$CPPFLAGS" \
-	CFLAGS="$DEBUG_CFLAGS -Wall -Werror $CFLAGS" \
-	CXXFLAGS="$DEBUG_CXXFLAGS -Wall -Werror $CXXFLAGS" \
+	CFLAGS="$DEBUG_CFLAGS -Wall -Wextra -Werror $CFLAGS" \
+	CXXFLAGS="$DEBUG_CXXFLAGS -Wall -Wextra -Werror $CXXFLAGS" \
 	LDFLAGS="$LDFLAGS" \
 	DEBUG_CFLAGS="$DEBUG_CFLAGS" \
 	DEBUG_CXXFLAGS="$DEBUG_CXXFLAGS"
 
-# cscope target won't work without this
-#
-[ -f po/Makefile ] && echo -e '\n%:\n\t@:\n\n' >> po/Makefile
